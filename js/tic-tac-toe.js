d0370898b04cffafa25616261614adfe1cb70c03
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-04-04 10:08:32 +1000

    declared new variables to store player 1 and player 2, the board and each square on the 3x3 grid

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -2,28 +2,38 @@
 
 /*
 
-Create a loop that will continue until either a player has won or the board is full.
+Add event listeners to the game board squares to handle clicks by the players.
+Create a function to update the game state based on player input.
+Write a function to check for a win condition, and update the game state accordingly.
+Create a function to switch players after each move.
+Add code to handle the end of the game, either by a win or a draw.
+Implement a reset button to start a new game.
 
-Inside the loop, determine which player's turn it is (either X or O).
-
-Ask the current player to choose a row and column on the board to place their symbol (either X or O).
-
-Check if the chosen position on the board is empty. If it is not empty, ask the player to choose a different position.
-
-If the position is empty, update the board with the player's symbol in the chosen row and column.
-
-Check if the player has won the game by having three of their symbols in a row horizontally, vertically, or diagonally.
-
-If the player has won, display a message announcing their victory and end the loop.
-
-If the board is full and no player has won, display a message announcing a tie game and end the loop.
-
-If neither player has won and the board is not full, switch to the other player's turn and continue the loop.
-
-Once the loop has ended (because a player has won or the board is full), ask the players if they want to play again. If they do, start a new game with an empty board. If they do not, end the program.
 
 */
 
-//Initialising the board with nested arrays where each square on the board represents a spot on the 3x3 grid. 
+//Declaring a new variable for player 1
+
+const player1 = '😼';
+
+//Declaring a new variable for player 2
+
+const player2 = '🐶';
+
+//Declaring a new variable for the board
 
 const board = [ ['_', '_', '_'],['_', '_', '_']['_', '_', '_'] ];
+
+//Creating an object by initialising each square to an underscore. 
+
+const squares = {
+    'square1-1': '_',
+    'square1-2': '_',
+    'square1-3': '_',
+    'square2-1': '_',
+    'square2-2': '_',
+    'square2-3': '_',
+    'square3-1': '_',
+    'square3-2': '_',
+    'square3-3': '_',
+};
